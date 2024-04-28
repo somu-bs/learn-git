66437b4aabc44258706545e4a466b8c4456ba258
Somnath Chakraborty <somnathc352@gmail.com>
2024-04-28 17:30:44 +0530

    add new function in index file

--- Index.java
+++ Index.java
@@ -1,5 +1,12 @@
 public class Index {
     public static void main(String[] args) {
-        System.out.printf("Just started working on a new folder");
+        System.out.println("Just started working on a new folder");
+
+        int sum = doSum(2, 5);
+        System.out.println(sum);
+    }
+
+    public static int doSum(int a, int b) {
+        return a+b;
     }
 }
